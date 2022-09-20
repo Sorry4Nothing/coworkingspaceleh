7724f67873d77a4ed9e7108a32d49939615efd81
Sorry4Nothing <robnoleh@gmail.com>
2022-09-20 13:04:00 +0200

    changed path

--- src/main/java/com/lehmann/coworkingspaceleh/configuration/OpenApiConfig.java
+++ src/main/java/com/lehmann/coworkingspaceleh/configuration/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .packagesToScan("io.viascom.springbootcrudexample.controller")
+                .packagesToScan("com.lehmann.coworkingspaceleh.controller")
                 .group("crud-api")
                 .build();
     }
